d89153284345d575d7ac15924fbefad8c04f26a0
sonhaaa <nguyensonha.hanz@gmail.com>
2022-01-03 20:14:42 +0700

    commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+# Demo Portal
+
+## Setup
+
+Download [Node.js](https://nodejs.org/en/download/).
+Run this followed commands:
+
+```bash
+# Install dependencies (only the first time)
+npm install
+
+# Run the local server at localhost:8080
+npm run dev
+
+# Build for production in the dist/ directory
+npm run build
+```
+
+Check out https://pensive-goldstine-114508.netlify.app/ for demo.
+
+There're a bunch of mistake such as: wrong color, wrong position, etc.
---
+++ src/script.js
@@ -0,0 +1,214 @@
+import './style.css'
+// import * as dat from 'dat.gui'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+
+import firefliesVertexShader from './shaders/fireflies/vertex.glsl'
+import firefliesFragmentShader from './shaders/fireflies/fragment.glsl'
+
+import portalVertexShader from './shaders/portal/vertex.glsl'
+import portalFragmentShader from './shaders/portal/fragment.glsl'
+
+/**
+ * Base
+ */
+// Debug
+// const debugObject = {}
+// const gui = new dat.GUI({
+//     width: 400
+// })
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Loaders
+ */
+// Texture loader
+const textureLoader = new THREE.TextureLoader()
+
+// Draco loader
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('draco/')
+
+// GLTF loader
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+/**
+ * Textures
+ */
+const backedTexture = textureLoader.load(
+    'myModels/baked.jpg'
+)
+backedTexture.flipY = false
+backedTexture.encoding = THREE.sRGBEncoding
+
+/**
+ * Material
+ */
+// Baked
+const bakedMaterial = new THREE.MeshBasicMaterial({ map: backedTexture })
+
+// Portal light
+const portalLightMaterial = new THREE.ShaderMaterial({ 
+    vertexShader: portalVertexShader,
+    fragmentShader: portalFragmentShader,
+    uniforms: {
+        uTime: { value: 0 },
+        uColorStart: { value: new THREE.Color(0x000000) },
+        uColorEnd: { value: new THREE.Color(0xffffff) },
+    },
+ })
+
+// Polelight
+const poleLightMaterial = new THREE.MeshBasicMaterial({ color: 0xffffe5 })
+
+
+/**
+ * Model
+ */
+gltfLoader.load(
+    'myModels/portal.glb',
+    (gltf) => {
+        gltf.scene.traverse((child) => {
+            child.material = bakedMaterial
+        })
+
+        const portalLightMesh = gltf.scene.children.find((child) => child.name === 'Circle')
+        const poleLightAMesh = gltf.scene.children.find((child) => child.name === 'Cube011')
+        const poleLightBMesh = gltf.scene.children.find((child) => child.name === 'Cube015')
+
+        poleLightAMesh.material = poleLightMaterial
+        poleLightBMesh.material = poleLightMaterial
+
+        portalLightMesh.material = portalLightMaterial
+
+        scene.add(gltf.scene)
+    }
+)
+// Circle Cube.011 Cube.015
+
+/**
+ * Fireflies
+ */
+// geometry
+const firefliesGeometry = new THREE.BufferGeometry()
+const firefliesCount = 30
+const positionArray = new Float32Array(firefliesCount * 3)
+const scaleArray = new Float32Array(firefliesCount)
+
+for (let i = 0; i < firefliesCount; i++) {
+    positionArray[i * 3 + 0] = (Math.random() - .5) * 4
+    positionArray[i * 3 + 1] = Math.random() * 1.5
+    positionArray[i * 3 + 2] = (Math.random() - .5) * 4
+    
+    scaleArray[i] = Math.random()
+}
+
+firefliesGeometry.setAttribute('position', new THREE.BufferAttribute(positionArray, 3))
+firefliesGeometry.setAttribute('aScale', new THREE.BufferAttribute(scaleArray, 1))
+
+// Material
+const firefliesMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+        uPixelRatio: { value: Math.min(window.devicePixelRatio, 2) },
+        uSize: { value: 200 },
+        uTime: { value: 0 },
+    },
+    vertexShader: firefliesVertexShader,
+    fragmentShader: firefliesFragmentShader,
+    transparent: true,
+    blending: THREE.AdditiveBlending,
+    depthWrite: false,
+})
+
+// Point
+const fireflies = new THREE.Points(firefliesGeometry, firefliesMaterial)
+scene.add(fireflies)
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    // Update fireflies
+    fireflies.uniforms.uPixelRatio = Math.min(window.devicePixelRatio, 2)
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 4
+camera.position.y = 2
+camera.position.z = 4
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.outputEncoding = THREE.sRGBEncoding
+
+// debugObject.clearColor = '#202222'
+// renderer.setClearColor(debugObject.clearColor)
+// gui.addColor(debugObject, 'clearColor').onChange(() => renderer.setClearColor(debugObject.clearColor))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update material 
+    firefliesMaterial.uniforms.uTime.value = elapsedTime
+    portalLightMaterial.uniforms.uTime.value = elapsedTime
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
